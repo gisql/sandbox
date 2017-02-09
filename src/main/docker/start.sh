21b0708526b480b345390652fa8266395417002b
Michal Ostapowicz <michal@clearscore.com>
2017-02-09 13:21:28 +0000

    alpine uses some non-standard tar, moving to zip

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
-. ./cs-setup.sh
-
 /usr/bin/consul-template -consul consul.service.consul:8500 -once -template \
-    '/logging-aws.xml.ctmpl:/logging-aws.xml:cat /logging-aws.xml' | log_filter &
+    '/logging-aws.xml.ctmpl:/logging-aws.xml:cat /logging-aws.xml' | java -jar /logger.jar &
 
 /usr/bin/envconsul -consul consul.service.consul:8500 -prefix config -prefix service/sandbox/config \
     /usr/bin/java -Dlogback.configurationFile=/logging-aws.xml -jar /sandbox.jar
