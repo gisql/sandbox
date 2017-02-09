5f9e598a4e5c5bc4023cd152955d36321ee3bb00
Michal Ostapowicz <michal@clearscore.com>
2017-02-09 12:03:45 +0000

    adding second process to the mix

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
+/usr/bin/envconsul -consul consul.service.consul:8500 -prefix service/sandbox/logging \
+    /bin/bash dummy.sh &
+
 /usr/bin/envconsul -consul consul.service.consul:8500 -prefix config -prefix service/sandbox/config \
     /usr/bin/java -Dlogback.configurationFile=/logging-aws.xml -jar /sandbox.jar
