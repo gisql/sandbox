d79e815fd92b1639dba532cd7147e3cf584a4eb7
Michal Ostapowicz <michal@clearscore.com>
2017-02-09 13:08:53 +0000

    attempt to start log configuration refresher in background

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-/usr/bin/envconsul -consul consul.service.consul:8500 -prefix service/sandbox/logging \
-    /bin/bash dummy.sh &
+. ./cs-setup.sh
+
+/usr/bin/consul-template -consul consul.service.consul:8500 -once -template \
+    '/logging-aws.xml.ctmpl:/logging-aws.xml:cat /logging-aws.xml' | log_filter &
 
 /usr/bin/envconsul -consul consul.service.consul:8500 -prefix config -prefix service/sandbox/config \
     /usr/bin/java -Dlogback.configurationFile=/logging-aws.xml -jar /sandbox.jar
