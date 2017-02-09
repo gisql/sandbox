d36d6b878d05191595c9c9e410f6fdf8a60efbc9
Michal Ostapowicz <michal@clearscore.com>
2017-02-09 14:34:52 +0000

    using dynamic logger from base image

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-/usr/bin/consul-template -consul consul.service.consul:8500 -template \
-    '/logging-aws.xml.ctmpl:/logging-aws.xml:cat /logging-aws.xml' | java -jar /logger.jar &
+/cs-dynamic-log.sh 'sandbox'
 
 /usr/bin/envconsul -consul consul.service.consul:8500 -prefix config -prefix service/sandbox/config \
     /usr/bin/java -Dlogback.configurationFile=/logging-aws.xml -jar /sandbox.jar
